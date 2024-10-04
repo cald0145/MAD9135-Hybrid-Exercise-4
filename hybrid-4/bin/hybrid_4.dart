f04e83410cf3b04632bf3c9052e8182a09e5e7cf
Jay Calderon <cald0145@algonquinlive.com>
2024-10-04 01:15:18 -0400

    Refactor main function to make async HTTP requests and parse JSON data

--- hybrid-4/bin/hybrid_4.dart
+++ hybrid-4/bin/hybrid_4.dart
@@ -1,7 +1,33 @@
 import 'package:hybrid_4/hybrid_4.dart' as hybrid_4;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+//importing packages for json parsing and http requests
 
-void main(List<String> arguments) {
-  print('Hello world: ${hybrid_4.calculate()}!');
+void main() async {
+  //async await
+  const int numberOfUsers = 10;
+  const String apiUrl = 'https://random-data-api.com/api/v2/users?size=$numberOfUsers';
+
+  try {
+    //using try catch to handle errors for the http requests
+    //using get and await to make the http request
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      //checking status code if OK then parse the json and 
+      // use json decode to parse the data into a list of objects!
+      final List<dynamic> users = json.decode(response.body);
+
+      for (var user in users) {
+        //looping through the list of users and printing each uid and name
+        print('${user['uid']} - ${user['first_name']} ${user['last_name']}');
+      }
+    } else {
+      //if status code is not 200 then print the status code and error message! :^)
+      print('Failed to fetch data. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    //catch error in block
+    print('An error occurred: $e');
+  }
 }
