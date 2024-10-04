03497afed2432fa099bd99696dadc8048b7d3336
Jay Calderon <cald0145@algonquinlive.com>
2024-10-04 01:09:37 -0400

    Init dart project for console template and add http dependency

---
+++ hybrid-4/bin/hybrid_4.dart
@@ -0,0 +1,7 @@
+import 'package:hybrid_4/hybrid_4.dart' as hybrid_4;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid_4.calculate()}!');
+}
